d8ef0a786514b5ef984c8054bfa3dc9c66341ce9
Abisheikfeb <143818859+Abisheikfeb@users.noreply.github.com>
2024-10-19 22:21:56 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import Hero from './components/hero/hero'
+import Hero from './components/Hero/Hero'
 import About from './components/About/About'
+import MultiFilters from './components/skils/MultiFilters'
+import Project from './components/project/project'
+import ContactUs from './components/connect/ContactUs'
 
 
+function App(){
+  
 
-export const App = () => {
   return (
+    
     <div>
       <Navbar/>
       <Hero/>
       <About/>
-      
-      
+      <MultiFilters/>
+      <Project/>
+      <ContactUs/>
 
     </div>
   )
 }
-export default App
+export default App;
